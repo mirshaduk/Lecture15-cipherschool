fe01f6d43b7120dbb68fb7a95945896ce48f9c50
mirshaduk <119918613+mirshaduk@users.noreply.github.com>
2024-07-08 10:51:49 +0530

    Add files via upload

---
+++ morefunction1.cpp
@@ -0,0 +1,16 @@
+int compare(int a, int b) 
+
+{
+    int c;
+    if(a>b)
+    {
+        return a;
+
+    }
+    wlse
+    
+    {
+        return b;
+    }
+
+}
---
+++ morefunctions.cpp
@@ -0,0 +1,18 @@
+int compare(int a, int b) //comprae two integers and return the greatest
+
+{
+    int c;
+    if(a>b)
+    {
+        c=a;
+
+    }
+    wlse
+    
+    {
+        c=b;
+    }
+    // now c will store maximum of a,b
+
+    return c;
+}
